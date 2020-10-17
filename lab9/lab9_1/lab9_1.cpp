d67737f7b2f26f6fa5c6da2f8c7b2d8fd57dabe1
Mafiya14 <Mafiya14@users.noreply.github.com>
2020-10-17 14:31:15 +0300

    lab9_1 complete

---
+++ lab9/lab9_1/lab9_1.cpp
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	int N;
+	setlocale(LC_ALL, "Rus"); //функция локализации на русский язык
+	printf("введите N>>"); scanf_s("%d", &N);
+	printf("%d", N % 60);
+	return (0);
+}
