479c528535c51731f5dc7b3aa98e31a7a2432f3a
Mafiya14 <Mafiya14@users.noreply.github.com>
2020-10-17 13:52:07 +0300

    lab8_1 processing

---
+++ lab8/lab8_1/lab8_1.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	int a, b;
+	setlocale(LC_ALL, "Rus"); //функция локализации на русский язык
+	printf("введите размер файла>>"); scanf_s("%d", &a);
+	if (a % 1024 == 0) {
+		b = a / 1024;
+		printf("%d", (a / 1024));
+	}
+	else printf("%d", a / 1024 + 1);
+	return(0);
+}
