b44ffd2dee11cd10c4753eb0631c80bf7fcc99ce
Mafiya14 <Mafiya14@users.noreply.github.com>
2020-10-17 14:06:10 +0300

    lab8_3 complete

---
+++ lab8/lab8_3/lab8_3.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	float a, b;
+	setlocale(LC_ALL, "Rus"); //функция локализации на русский язык
+	printf("введите A B>>"); scanf_s("%f%f", &a, &b);
+	while (a >= b) 
+		a = a - b;
+	printf("%f", a);
+	return(0);
+}
