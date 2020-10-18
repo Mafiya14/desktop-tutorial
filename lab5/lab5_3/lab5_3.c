70dd45961b10485f959fe5c1dd21bb3fe2bff99e
Mafiya14 <Mafiya14@users.noreply.github.com>
2020-10-18 23:36:48 +0300

    lab5_3 done

---
+++ lab5/lab5_3/lab5_3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+main()
+{
+	int a, b, c, Lac, Lbc;
+	setlocale(LC_ALL, "Rus");
+	printf("введите координаты точки А>>"); scanf_s("%d", &a);
+	printf("введите координаты точки В>>"); scanf_s("%d", &b);
+	printf("введите координаты точки С>>"); scanf_s("%d", &c);
+	Lac = abs(a - c);
+	Lbc = abs(b - c);
+	printf("Длина AC равна %d\n", Lac);
+	printf("Длина BC равна %d\n", Lbc);
+	printf("Произведение отрезков равно %d", Lac * Lbc);
+}
