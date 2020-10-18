d20d2fcb8a8d3b4d967aaa641dc53aa89a27b087
Mafiya14 <Mafiya14@users.noreply.github.com>
2020-10-18 23:46:43 +0300

    lab5_4 done

---
+++ lab5/lab5_4/lab5_4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+main()
+{
+	int x1, x2, y1, y2, P, S, a, b;
+	setlocale(LC_ALL, "Rus");
+	printf("введите координаты точки А>>"); scanf_s("%d%d", &x1, &y1);
+	printf("введите координаты точки В>>"); scanf_s("%d%d", &x2, &y2);
+	a = abs(x2 - x1); b = abs(y2 - y1);
+	P = 2 * (a + b); S = a * b;
+	printf("Периметр равен %d\n", P);
+	printf("Площадь равна %d\n", S);
+}
