327b6bbac5eae88c1ce085c08fa8f6a15232d543
Mafiya14 <Mafiya14@users.noreply.github.com>
2020-10-24 23:28:39 +0300

    lab10_1 done

---
+++ lab10/lab10_1/lab10_1.c
@@ -0,0 +1,11 @@
+﻿//Даны два целых числа: A, B. Проверить истинность высказывания: «Справедливы неравенства A > 2 и B ≤ 3»
+#include <stdio.h>
+#include <locale.h>
+int main() {
+	int a, b;
+	setlocale(LC_ALL, "Rus");
+	printf("введите a и b>>"); scanf_s("%d%d", &a, &b);
+	if ((a > 2) && (b <= 3)) printf("yes");
+	else printf("no");
+	return 0;
+}
