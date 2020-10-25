353d99bd1eef8a05e6e23575af9e0f435f184313
Mafiya14 <Mafiya14@users.noreply.github.com>
2020-10-25 16:12:22 +0300

    done

--- lab10/lab10_5/lab10_5.cpp
+++ lab10/lab10_5/lab10_5.cpp
@@ -2,7 +2,14 @@
 #include <stdio.h>
 #include <locale.h>
 int main() {
-	int ch;
+	int ch, a, b, c, d;
 	setlocale(LC_ALL, "Rus");
-	printf(">>"); scanf_s("%d", &ch);
+	printf("введите четырехзначное число>>"); scanf_s("%d", &ch);
+	a = ch / 1000; //first
+	b = ch / 100 % 10; //second
+	c = ch % 100 / 10; //third
+	d = ch % 10; //fourth
+	if ((a == d) && (b == c)) printf("yes");
+	else printf("no");
+	return 0;
 }
