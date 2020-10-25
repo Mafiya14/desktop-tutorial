d77775a8de6b27e2b396c744c85c64dfca88d7d2
Mafiya14 <Mafiya14@users.noreply.github.com>
2020-10-25 16:06:38 +0300

    done

--- lab10/lab10_7/lab10_7.c
+++ lab10/lab10_7/lab10_7.c
@@ -4,7 +4,7 @@
 int main() {
 	int a, b, c;
 	setlocale(LC_ALL, "Rus");
-	printf(">>"); scanf_s("%d%d%d", &a, &b, &c);
+	printf("¬ведите три целых числа>>"); scanf_s("%d%d%d", &a, &b, &c);
 	if ((a + b > c) && (b + c > a) && (c + a > b)) printf("yes");
 	else printf("no");
 	return 0;
