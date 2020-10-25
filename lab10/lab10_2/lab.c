f75b1e2f81be91128ac9ab1b338f71d147dccb74
Mafiya14 <Mafiya14@users.noreply.github.com>
2020-10-25 11:50:42 +0300

    lab

--- lab10/lab10_2/lab.c
+++ lab10/lab10_2/lab.c
@@ -1,7 +1,8 @@
 //Даны три целых числа: A, B, C. Проверить истинность высказывания: «Справедливо двойное неравенство A < B < C».
 #include <stdio.h>
 #include <locale.h>
-int main() {
+int main()
+{
 	int a, b, c;
 	setlocale(LC_ALL, "Rus");
 	printf("Ввделите 3 целых числа>>"); scanf_s("%d%d%d", &a, &b, &c);
