97cd89b2da9f182a284508298bcb53fccc6bc13a
Mafiya14 <Mafiya14@users.noreply.github.com>
2021-01-02 14:54:53 +0300

    lab11_4 done

---
+++ lab11/lab11_4/lab11_4.c
@@ -0,0 +1,12 @@
+//Даны координаты точки, не лежащей на координатных осях OX и OY. Определить номер координатной четверти, в которой находится данная точка
+#include <stdio.h>
+#include <math.h>
+int main() {
+	int x, y;
+	printf("coordinates x and y>>"); scanf_s("%d%d", &x, &y);
+	if ((x > 0) && (y > 0)) printf("first");
+	if ((x > 0) && (y < 0)) printf("second");
+	if ((x < 0) && (y < 0)) printf("third");
+	if ((x < 0) && (y > 0)) printf("forth");
+	return 0;
+}
