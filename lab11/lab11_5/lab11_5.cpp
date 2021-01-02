af7a0d2325974580931afd2d5e94fdfb564f110b
Mafiya14 <Mafiya14@users.noreply.github.com>
2021-01-02 22:00:05 +0300

    lab11_5 done

---
+++ lab11/lab11_5/lab11_5.cpp
@@ -0,0 +1,18 @@
+﻿//Дано целое число. Вывести его строку-описание вида «отрицательное четное число», «нулевое число», «положительное нечетное число» и т. д.
+#include <stdio.h>
+#include <locale.h>
+int main() {
+	int x;
+	setlocale(LC_ALL, "Rus");
+	printf("введите число>>"); scanf_s("%d", &x);
+	if (x == 0) printf("нулевое");
+	else 
+	{
+		if (x > 0) printf("положительное ");
+		else printf("отрицательное ");
+		if (x % 2 == 0) printf("четное");
+		else printf("нечетное");
+	}
+	printf(" число");
+	return 0;
+}
