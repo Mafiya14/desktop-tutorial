bf98c22078f77109fcdd26d5b9be918c6a4d27cb
Mafiya14 <Mafiya14@users.noreply.github.com>
2021-01-04 10:06:08 +0300

    lab13_2 done

---
+++ lab13/lab13_2/lab13_2.cpp
@@ -0,0 +1,20 @@
+﻿//Дано целое число N (> 0). Найти произведение 1.1 · 1.2 · 1.3 · . . . (N сомножителей).
+#include <iostream>
+#include <locale.h>
+using namespace std;
+
+int main()
+{
+	int i, N;
+	float S;
+	setlocale(LC_ALL, "Rus");
+	cout << "введите число>>";
+	cin >> N;
+	S = 1;
+	for (i = 1; i <= N; i++)
+	{
+		S = S * ( 1 + 0.1 * i );
+	}
+	cout << S;
+	return 0;
+}
