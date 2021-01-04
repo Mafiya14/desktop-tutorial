140de0a947352bf010cff34c54933bb303350357
Mafiya14 <Mafiya14@users.noreply.github.com>
2021-01-04 21:48:15 +0300

    lab14_3 done

---
+++ lab14/lab14_3/lab14_3.cpp
@@ -0,0 +1,21 @@
+﻿//Дано целое число N (> 1). Вывести наименьшее из целых чисел K, для которых сумма 1 + 2 + . . . + K будет больше или равна N, и саму эту сумму.
+#include <iostream> 
+#include <locale.h> 
+using namespace std;
+
+int main()
+{
+	int N, K, S;
+	setlocale(LC_ALL, "Rus");
+	cout << "введите число N>>";
+	cin >> N;
+	K = 0;
+	S = 0;
+	while (N > S)
+	{
+		K = K + 1;
+		S = S + K;
+	}
+	cout << K << " " << S;
+	return 0;
+}
