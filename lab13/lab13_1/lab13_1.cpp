f9f0c8538e69496901bdd958093c496b5a596c7e
Mafiya14 <Mafiya14@users.noreply.github.com>
2021-01-04 10:00:10 +0300

    lab13_1 done

---
+++ lab13/lab13_1/lab13_1.cpp
@@ -0,0 +1,18 @@
+﻿//Дано вещественное число — цена 1 кг конфет. Вывести стоимость 0.1, 0.2, . . . , 1 кг конфет.
+#include <iostream>
+#include <locale.h>
+using namespace std;
+
+int main()
+{
+	float c;
+	int i;
+	setlocale(LC_ALL, "Rus");
+	cout << "введите цену за 1 кг конфет>>";
+	cin >> c;
+	for (i = 1; i <= 10; i++)
+	{
+		cout << 0.1 * i << " кг = " << c * 0.1 * i << "\n";
+	}
+	return 0;
+}
