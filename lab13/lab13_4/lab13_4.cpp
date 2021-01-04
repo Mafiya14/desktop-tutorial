95b1b69dc7a1e913e5dd5adc5229cbb66777a50f
Mafiya14 <Mafiya14@users.noreply.github.com>
2021-01-04 10:16:46 +0300

    lab13_4 done

---
+++ lab13/lab13_4/lab13_4.cpp
@@ -0,0 +1,20 @@
+﻿//Дано вещественное число A и целое число N (> 0). Используя один цикл, найти сумму 1 + A + A^2 + A^3 + . . . + A^N
+#include <iostream>
+#include <locale.h>
+using namespace std;
+
+int main()
+{
+	int i, N;
+	float A, S;
+	setlocale(LC_ALL, "Rus");
+	cout << "введите вещественное число А и целое число N>>";
+	cin >> A >> N;
+	S = 1;
+	for (i = 1; i <= N; i++)
+	{
+		S = S + pow(A, i);
+	}
+	cout << S;
+	return 0;
+}
