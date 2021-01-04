74d946e9b7b08e33110934f6b0e5f83d956cdb1f
Mafiya14 <Mafiya14@users.noreply.github.com>
2021-01-04 22:18:23 +0300

    lab14_5 done

---
+++ lab14/lab14_5/lab14_5.cpp
@@ -0,0 +1,19 @@
+﻿//Даны целые положительные числа A и B. Найти их наибольший общий делитель (НОД), используя алгоритм Евклида
+#include <iostream> 
+#include <locale.h> 
+using namespace std;
+
+int main()
+{
+	int A, B;
+	setlocale(LC_ALL, "Rus");
+	cout << "введите A и B >>";
+	cin >> A >> B;
+	do 
+	{
+		if (A > B) A = A % B;
+		else B = B % A;
+	} while ((A != 0) and (B != 0));
+	cout << (A + B);
+	return 0;
+}
