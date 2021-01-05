4275d3d2a6ab2354c5666198a43d53686530bfba
Mafiya14 <Mafiya14@users.noreply.github.com>
2021-01-05 18:07:03 +0300

    lab16_4 done

---
+++ lab16/lab16_4/lab16_4.cpp
@@ -0,0 +1,31 @@
+﻿//Дан массив A размера N. Вывести его элементы в следующем порядке: A(1), A(N) , A(2), A(N−1), A(3), A(N−2),
+#include <iostream> 
+#include <locale.h> 
+using namespace std;
+
+int main()
+{
+	int N, k, i;
+	setlocale(LC_ALL, "Rus");
+	k = 0;
+	cout << "введите N>>";
+	cin >> N;
+	int *A = new int[N];
+	cout << "введите элементы массива>>";
+	for (i = 0; i < N; i++)
+	{
+		cin >> A[i];
+	}
+	for (i = 0; i < N; i++)
+	{
+
+		if (i % 2 == 0) cout << A[k] << "\n";
+		else
+		{
+			cout << A[N - k -1] << "\n";
+			k++;
+		}
+	}
+	delete[] A; // очистка памяти
+	return 0;
+}
