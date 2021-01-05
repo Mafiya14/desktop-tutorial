05ef140ecf717ff678166b9337bde7dc9f6c8e28
Mafiya14 <Mafiya14@users.noreply.github.com>
2021-01-05 17:14:16 +0300

    lab16_1 done

---
+++ lab16/lab16_1/lab16_1.cpp
@@ -0,0 +1,21 @@
+﻿//Дано целое число N (> 0). Сформировать и вывести целочисленный массив размера N, содержащий N первых положительных нечетных чисел: 1, 3, 5,...
+#include <iostream> 
+#include <locale.h> 
+using namespace std;
+
+int main()
+{
+	int N, l;
+	setlocale(LC_ALL, "Rus");
+	l = 1;
+	cout << "введите N>>";
+	cin >> N;
+	int *mass = new int[N];
+	for (int i = 0; i < N; i++)
+	{
+		mass[i] = l;
+		l = l + 2;
+		cout << mass[i] << "\n";
+	}
+	return 0;
+}
