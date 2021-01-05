af70cb3cd26ed4a99688ebfdc2d7440202030ae0
Mafiya14 <Mafiya14@users.noreply.github.com>
2021-01-05 15:12:00 +0300

    lab15_2 done

---
+++ lab15/lab15_2/lab15_2.cpp
@@ -0,0 +1,26 @@
+﻿//Описать функцию Sign(X) целого типа, возвращающую для вещественного числа X следующие значения:
+//−1, если X < 0; 0, если X = 0; 1, если X > 0.
+#include <iostream> 
+#include <locale.h> 
+using namespace std;
+
+int Sign(float X)
+{
+	int s;
+	if (X < 0) s = -1;
+	if (X == 0) s = 0;
+	if (X > 0) s = 1;
+	return s;
+}
+
+int main()
+{
+	float X;
+	int s;
+	setlocale(LC_ALL, "Rus");
+	cout << "введите X>>";
+	cin >> X;
+	s = Sign(X);
+	cout << s;
+	return 0;
+}
