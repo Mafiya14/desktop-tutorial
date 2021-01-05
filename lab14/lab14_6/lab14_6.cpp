4774688aa72ea5f1f98bce734b5406bfbdeffe4f
Mafiya14 <Mafiya14@users.noreply.github.com>
2021-01-05 13:14:56 +0300

    done

---
+++ lab14/lab14_6/lab14_6.cpp
@@ -0,0 +1,21 @@
+﻿//Дано целое число N (> 1), являющееся числом Фибоначчи: N = Fk. Найти целое число K — порядковый номер числа Фибоначчи N.
+#include <iostream> 
+#include <locale.h> 
+using namespace std;
+
+int main()
+{
+	int N, K, a, b; //a, b - два рядом стоящие числа
+	setlocale(LC_ALL, "Rus");
+	cout << "введите N >>";
+	cin >> N;
+	a = 0; b = 1; K = 2;
+	while (N != a && N != b)
+	{
+		K = K + 1;
+		if (K % 2 == 0) b = a + b;
+		else a = b + a;
+	}
+	cout << K;
+	return 0;
+}
