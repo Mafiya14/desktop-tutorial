f4e96e5a02d8c8d11acd3f7c632f702e6d1e98da
Mafiya14 <Mafiya14@users.noreply.github.com>
2021-01-05 16:02:53 +0300

    lab15_3 done

--- lab15/lab15_3/lab15_3.cpp
+++ lab15/lab15_3/lab15_3.cpp
@@ -2,25 +2,28 @@
 //и радиусами R1 и R2 (R1 и R2 — вещественные, R1 > R2). С ее помощью найти площади трех колец, для которых даны внешние и внутренние радиусы.
 #include <iostream> 
 #include <locale.h> 
+const double PI = 3.141592653589793;
+
 using namespace std;
 
-float RingS(float X)
+float RingS(float R1, float R2)
 {
-	int s;
-	if (X < 0) s = -1;
-	if (X == 0) s = 0;
-	if (X > 0) s = 1;
+	float s;
+	s = (pow(R1, 2) - pow(R2, 2)) * PI;
 	return s;
 }
 
 int main()
 {
-	float X;
-	int s;
+	float R1, R2, s;
+	int i;
 	setlocale(LC_ALL, "Rus");
-	cout << "введите X>>";
-	cin >> X;
-	s = Sign(X);
-	cout << s;
+	for (i = 1; i <= 3; i++)
+	{
+		cout << "введите R1 и R2>>";
+		cin >> R1 >> R2;
+		s = RingS(R1, R2);
+		cout << s << "\n";
+	}
 	return 0;
 }
