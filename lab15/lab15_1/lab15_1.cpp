9a38adbea042d8835d8583669c7285dc87b5acd8
Mafiya14 <Mafiya14@users.noreply.github.com>
2021-01-05 15:03:33 +0300

    lab15_1 done

---
+++ lab15/lab15_1/lab15_1.cpp
@@ -0,0 +1,23 @@
+﻿//Описать функцию PowerA3(A, B), вычисляющую третью степень числа A и возвращающую ее в переменной B (A — входной, B — выходной параметр; 
+//оба параметра являются вещественными). С помощью этой функции найти третьи степени пяти данных чисел.
+#include <iostream> 
+#include <locale.h> 
+using namespace std;
+
+float PowerA3 (float A)
+{
+	float B;
+	B = pow(A, 3);
+	return B;
+}
+
+int main()
+{
+	float A, B; 
+	setlocale(LC_ALL, "Rus");
+	cout << "введите A>>";
+	cin >> A;
+	B = PowerA3(A);
+	cout << B;
+	return 0;
+}
