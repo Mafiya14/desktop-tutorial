8570b79f77b3356bf86d803a7d5182c9b49cfc8f
Mafiya14 <Mafiya14@users.noreply.github.com>
2021-01-06 14:42:20 +0300

    lab17_2 done

---
+++ lab17/lab17_2/lab17_2.cpp
@@ -0,0 +1,27 @@
+﻿//Дан целочисленный массив размера N, не содержащий одинаковых чисел. Проверить, образуют ли его элементы арифметическую прогрессию. 
+//Если образуют, то вывести разность прогрессии, если нет — вывести 0.
+#include <iostream> 
+#include <locale.h> 
+using namespace std;
+
+int main()
+{
+	int N, d, i;
+	setlocale(LC_ALL, "Rus");
+	d = 0;
+	cout << "введите N>>";
+	cin >> N;
+	int *mass = new int[N];
+	cout << "введите элементы массива>>";
+	cin >> mass[0];
+	cin >> mass[1];
+	d = mass[1] - mass[0];
+	for (i = 2; i < N; i++)
+	{
+		cin >> mass[i];
+		if (d != mass[i] - mass[i - 1]) d = 0;
+	}
+	cout << d;
+	delete[] mass; // очистка памяти
+	return 0;
+}
