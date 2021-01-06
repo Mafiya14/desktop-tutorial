91311a8fa8b3de4f9f5c803ecf01b05b2360f099
Mafiya14 <Mafiya14@users.noreply.github.com>
2021-01-06 15:15:33 +0300

    lab17_3 done

---
+++ lab17/lab17_3/lab17_3.cpp
@@ -0,0 +1,27 @@
+﻿//Дан массив A размера N. Найти минимальный элемент из его элементов с четными номерами: A2, A4, A6,
+#include <iostream> 
+#include <locale.h> 
+using namespace std;
+
+int main()
+{
+	int N, min, i;
+	setlocale(LC_ALL, "Rus");
+	
+	cout << "введите N>>";
+	cin >> N;
+	int* A = new int[N];
+	cout << "введите элементы массива>>";
+	for (i = 0; i < N; i++)
+	{
+		cin >> A[i];
+	}
+	min = A[0];
+	for (i = 2; i < N; i = i + 2)
+	{
+		if (min > A[i]) min = A[i];
+	}
+	cout << min;
+	delete[] A; // очистка памяти
+	return 0;
+}
