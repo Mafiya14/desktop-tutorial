114a10fffb5a8f2e080760ddcb3f27cc53ae9b8f
Mafiya14 <Mafiya14@users.noreply.github.com>
2021-01-06 19:01:31 +0300

    done

---
+++ lab18/lab18_5/lab18_5.cpp
@@ -0,0 +1,33 @@
+﻿//Дан массив размера N, все элементы которого, кроме первого, упорядочены по возрастанию.Сделать массив упорядоченным, переместив первый элемент на новую позицию.
+#include <iostream> 
+#include <locale.h> 
+using namespace std;
+
+int main()
+{
+	int N, i, t, j;
+	setlocale(LC_ALL, "Rus");
+	j = 0;
+	cout << "введите N>>";
+	cin >> N;
+	int* A = new int[N];
+	cout << "введите элементы массива A>>";
+	for (i = 0; i < N; i++)
+	{
+		cin >> A[i];
+		if (A[j] > A[i])
+		{
+			t = A[i];
+			A[i] = A[j];
+			A[j] = t;
+			j = i;
+		}
+	}
+	cout << "измененный массив A>>";
+	for (i = 0; i < N; i++)
+	{
+		cout << A[i] << " ";
+	}
+	delete[] A; // очистка памяти
+	return 0;
+}
