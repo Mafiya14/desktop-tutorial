4eb3f81b56c4950424a60696b71482fbcbf32ace
Mafiya14 <Mafiya14@users.noreply.github.com>
2021-01-06 16:21:09 +0300

    lab17_5 done

---
+++ lab17/lab17_5/lab17_5.cpp
@@ -0,0 +1,36 @@
+﻿//Дан целочисленный массив размера N, содержащий ровно два одинаковых элемента. Найти номера одинаковых элементов и вывести эти номера в порядке возрастания
+#include <iostream> 
+#include <locale.h> 
+using namespace std;
+
+int main()
+{
+	int N, i;
+	bool flag;
+	setlocale(LC_ALL, "Rus");
+	flag = 0;
+	cout << "введите N>>";
+	cin >> N;
+	int* A = new int[N];
+	cout << "введите элементы массива>>";
+	for (i = 0; i < N; i++)
+	{
+		cin >> A[i];
+
+	}
+	i = 0;
+	do {
+		for (int j = 0; j < N; j++)
+		{
+			if (A[i] == A[j] && i != j)
+			{
+				if (i < j) cout << i << j;
+				else cout << j << i;
+				flag = 1;
+			}
+		}
+		i++;
+	} while (flag != 1);
+	delete[] A; // очистка памяти
+	return 0;
+}
