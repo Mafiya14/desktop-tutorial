37da787ed8628dbb632c3e640699649418fa2259
Mafiya14 <Mafiya14@users.noreply.github.com>
2021-01-10 12:46:38 +0300

    quicksort1

---
+++ алгоритмы сортировки/быстрая сортировка/quicksort1/quicksort1.cpp
@@ -0,0 +1,38 @@
+﻿//программная реализация быстрой сортировки через циклы с предусловием
+#include <stdio.h>
+#include <stdlib.h>
+int b; //индекс первого элемента массива
+int e; //индекс последнего элемента массива
+int l, r, arr[] = { -6, 1, 0, 5, 7, 8, 4, 9, -17, 12 };
+int n = sizeof(arr) / sizeof(int); 
+void qsort(int* arr, int b, int e) 
+{
+	if (b < e) 
+	{
+		int piv = arr[(b + e) / 2]; // номер опорного элемента
+		int l = b; //индекс левого элемента
+		int r = e; //индекс правого элемента
+		while (l <= r) 
+		{
+			while (arr[l] < piv) l++; 
+			while (arr[r] > piv) r--; 
+			if (l <= r) 
+			{
+				int t = arr[l]; 
+				arr[l] = arr[r]; 
+				arr[r] = t; 
+				l++; 
+				r--; 
+			}
+		}
+		qsort(arr, b, r); 
+		qsort(arr, l, e); 
+	}
+}
+
+int main()
+{
+		qsort(arr, 0, n - 1);
+	for (int i = 0; i < n - 1; i++)
+		printf("%d ", arr[i]);
+}
---
+++ алгоритмы сортировки/быстрая сортировка/quicksort2/quicksort2.cpp
@@ -0,0 +1,42 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+int b; //
+int e; //
+int l, r, t, arr[] = { -6, 1, 0, 5, 7, 8, 4, 9, -17, 12 };
+int n = sizeof(arr) / sizeof(int); 
+void qsort(int* arr, int b, int e)
+{
+	if (b < e) 
+	{
+		int piv = arr[(b + e) / 2];
+		int r = e;
+		for (l = b; l <= r; l++) 
+		{
+			for (int i = l; i < n; i++) 
+				if (arr[l] < piv) 
+					l++; 
+				else
+					break; 
+			for (int i = r; i > 0; i--) 
+				if (arr[r] > piv) 
+					r--; 
+				else
+					break; 
+			if (l <= r) 
+			{
+				int t = arr[l];
+				arr[l] = arr[r];
+				arr[r] = t;
+				r--;
+			}
+		}
+		qsort(arr, b, r); 
+		qsort(arr, l, e); 
+	}
+}
+int main()
+{
+		qsort(arr, 0, n - 1);	
+	for (int i = 0; i < n - 1; i++) 
+		printf("%d ", arr[i]);
+}
